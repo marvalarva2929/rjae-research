c19c988717e54f268f6daeeb7040e371afc4a471
marvalarva2929 <joshvigel329@outlook.com>
2024-12-20 23:01:35 -0600

    created model architecture

--- sa-okt/model.py
+++ sa-okt/model.py
@@ -1,29 +1,58 @@
-# layers needed:
-# 1: embedding layer
-# 2: multihead attention
-# 3: dropout
-# 4: normalization
-# 5: FFN (repeat 3, 4)
 
-# data flow
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
-# 1: input interaction sequence I: (N, x, d)
-#       N is # interactions in a sequence
-#       x is max length of an interaction
-#       d is embedding dimension
-#
-#       each interaction consists of
-#       (question, response, real_answer, correctness)
 
-# 2: feed emedded sequence I through attention layers
-#       I -> E
-#       might be hard to do this, each element of
-#       each sequence consists of multiple tensors
-#
-#       we have to connect x & d in such a way
-#       (concat question embds, response, etc) into Q and K
-#       or just concat the entire interaction into one tensor
+class SAOKT(nn.Module):
+    def __init__(self, latent_dim, max_seq_length, num_heads,
+                 num_apis, dropout, binary_out=False):
 
-# 3: feed E through FNN
-#       dropout + regularization
-#       train using cross-entropy
+        self.max_seq_length = max_seq_length
+        self.positional_embedding = nn.Embedding(
+                num_embeddings=max_seq_length+1, embedding_dim=latent_dim)
+
+        self.question_embedding = None  # GPT Encoder (text -> d/3)
+        self.question_embedding2 = None  # GPT Encoder 2 (text -> d)
+        self.response_embedding = nn.Embedding(num_embeddings=num_apis,
+                                               embedding_dim=(latent_dim)//3)
+        self.answer_embedding = nn.Embedding(num_embeddings=num_apis,
+                                             embedding_dim=latent_dim//3)
+
+        self.matn = nn.MultiheadAttention(
+                embed_dim=latent_dim, num_heads=num_heads, dropout=dropout)
+
+        self.ff1 = nn.Linear(in_features=latent_dim, out_features=latent_dim)
+        self.ff2 = nn.Linear(in_features=latent_dim, out_features=latent_dim)
+
+        self.prediction = nn.Linear(in_features=latent_dim,
+                                    out_features=max_seq_length)
+
+        self.layernorm = nn.LayerNorm(latent_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, question_sequence,
+                response_sequence, answer_sequence, is_embedded=False):
+
+        if not is_embedded:
+            question_sequence = self.question_embedding(question_sequence)
+            question_sequence2 = self.question_embedding2(question_sequence)
+            response_sequence = self.response_embedding(response_sequence)
+        answer_sequence = self.response_embedding(answer_sequence)
+
+        query = question_sequence2
+
+        x = torch.cat(question_sequence, response_sequence,
+                      answer_sequence, dim=-1)
+        x += self.positional_embedding(torch.arange(len(x)).unsqueeze(-1))
+
+        x = self.dropout(x)
+        future_mask = None
+        res = self.matn(query=query, key=x, value=x, future_masks=future_mask)
+        out = self.dropout(self.ff2(self.dropout(F.relu(self.ff1(res)))))
+
+        out += self.layernorm(res)
+        out = self.layernorm(out)
+
+        logits = self.prediction(out)
+        return logits
