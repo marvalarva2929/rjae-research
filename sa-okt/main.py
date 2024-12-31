4c1c6cd4da161f65dbb4d973980a392113673a08
marvalarva2929 <joshvigel329@outlook.com>
2024-12-31 01:27:15 -0600

    fixed main code pipeline

--- sa-okt/main.py
+++ sa-okt/main.py
@@ -1,9 +1,29 @@
 import pandas as pd
+from model import SAOKT
+import torch
+
+sequence_length = 50
+qdim = 128
+num_heads = 2
+num_apis = 50
+dropout = 0.02
 
 data_file = './small_dataset.pkl'
-dataset = pd.read_pickle(data_file)
 
 # Step 1: read data
 # Step 2: create model
 # Step 3: train model
 # Step 4: evaluate model
+
+
+m = SAOKT(qdim=qdim, max_seq_length=sequence_length,
+          num_heads=num_heads, num_apis=num_apis, dropout=dropout)
+
+dataset = pd.read_pickle(data_file)
+question_sequence = torch.randn(size=(sequence_length, qdim))
+response_sequence = torch.randn(size=(sequence_length, qdim))
+answer_sequence = torch.zeros(sequence_length).long()
+
+out = m.forward(question_sequence=question_sequence, response_sequence=response_sequence, answer_sequence=answer_sequence)
+print(out.shape)
+
--- sa-okt/model.py
+++ sa-okt/model.py
@@ -5,51 +5,55 @@ import torch.nn.functional as F
 
 
 class SAOKT(nn.Module):
-    def __init__(self, latent_dim, max_seq_length, num_heads,
+    def __init__(self, qdim, max_seq_length, num_heads,
                  num_apis, dropout, binary_out=False):
+        
+        super().__init__()
 
+        kdim = 3 * qdim
         self.max_seq_length = max_seq_length
         self.positional_embedding = nn.Embedding(
-                num_embeddings=max_seq_length+1, embedding_dim=latent_dim)
+                num_embeddings=max_seq_length+1, embedding_dim=kdim)
 
-        self.question_embedding = None  # GPT Encoder (text -> d/3)
-        self.question_embedding2 = None  # GPT Encoder 2 (text -> d)
+        self.question_embedding = None  # GPT Encoder (text -> d)
         self.response_embedding = nn.Embedding(num_embeddings=num_apis,
-                                               embedding_dim=(latent_dim)//3)
+                                               embedding_dim=qdim)
         self.answer_embedding = nn.Embedding(num_embeddings=num_apis,
-                                             embedding_dim=latent_dim//3)
+                                             embedding_dim=qdim)
 
         self.matn = nn.MultiheadAttention(
-                embed_dim=latent_dim, num_heads=num_heads, dropout=dropout)
+                embed_dim=qdim, num_heads=num_heads, dropout=dropout,
+                kdim=kdim, vdim=kdim)
 
-        self.ff1 = nn.Linear(in_features=latent_dim, out_features=latent_dim)
-        self.ff2 = nn.Linear(in_features=latent_dim, out_features=latent_dim)
+        self.ff1 = nn.Linear(in_features=qdim, out_features=qdim)
 
-        self.prediction = nn.Linear(in_features=latent_dim,
+        self.prediction = nn.Linear(in_features=qdim,
                                     out_features=max_seq_length)
 
-        self.layernorm = nn.LayerNorm(latent_dim)
+        self.layernorm = nn.LayerNorm(qdim)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, question_sequence,
-                response_sequence, answer_sequence, is_embedded=False):
+                response_sequence, answer_sequence, is_embedded=True):
 
         if not is_embedded:
             question_sequence = self.question_embedding(question_sequence)
-            question_sequence2 = self.question_embedding2(question_sequence)
             response_sequence = self.response_embedding(response_sequence)
         answer_sequence = self.response_embedding(answer_sequence)
 
-        query = question_sequence2
+        query = question_sequence
+        
+        print(response_sequence.shape, question_sequence.shape, answer_sequence.shape)
 
-        x = torch.cat(question_sequence, response_sequence,
-                      answer_sequence, dim=-1)
-        x += self.positional_embedding(torch.arange(len(x)).unsqueeze(-1))
+        x = torch.cat((question_sequence, response_sequence,
+                      answer_sequence), dim=-1)
+
+        x += self.positional_embedding(torch.arange(len(x)))
 
         x = self.dropout(x)
         future_mask = None
-        res = self.matn(query=query, key=x, value=x, future_masks=future_mask)
-        out = self.dropout(self.ff2(self.dropout(F.relu(self.ff1(res)))))
+        res, _ = self.matn(query=query, key=x, value=x)
+        out = self.dropout(F.relu(self.ff1(res)))
 
         out += self.layernorm(res)
         out = self.layernorm(out)
